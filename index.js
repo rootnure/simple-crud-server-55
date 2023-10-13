ce10c976d53f2de02acdc9e6e7e0d6874d6750ec
rootnure <nur.diu.2791@gmail.com>
2023-10-14 00:00:26 +0600

    MongoDB Create data code added

--- index.js
+++ index.js
@@ -27,12 +27,30 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // Connect to the "usersDB" database and access its "userCollection" collection
+        const usersDatabase = client.db("usersDB");
+        const userCollection = usersDatabase.collection("users");
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user', user);
+
+            // Insert the defined document into the "haiku" collection
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.log);
