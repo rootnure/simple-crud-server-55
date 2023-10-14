74a57f64b1a7e53a352c3b4117786f99f52174fd
rootnure <nur.diu.2791@gmail.com>
2023-10-14 11:05:45 +0600

    Read operation [ GET ] added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // nurdiu2791
 // KzEiqPUcssdfajud
@@ -28,17 +28,24 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        // Connect to the "usersDB" database and access its "userCollection" collection
-        const usersDatabase = client.db("usersDB");
-        const userCollection = usersDatabase.collection("users");
+        // Connect to the "usersDB" database and access its "users" collection
+        const userCollection = client.db('usersDB').collection('users');
 
+        // Read operation [ GET ]
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // Create operation [ POST ]
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log('new user', user);
 
-            // Insert the defined document into the "haiku" collection
+            // Insert the defined document into the "users" collection
             const result = await userCollection.insertOne(user);
-            res.send(result)
+            res.send(result);
 
         })
 
@@ -48,11 +55,13 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+    }
+    finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
+        // await client.close(); // comment-out to establish stable connection
     }
 }
+
 run().catch(console.log);
 
 
